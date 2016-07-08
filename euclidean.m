bd143e864cfa34ec7f2d9049807ae55bc9b07bdd
Charlotte <burmeisc@tcd.ie>
2016-07-08 11:32:16 +0900

    fixed wrong variable in euclidean causing wrong distances

--- euclidean.m
+++ euclidean.m
@@ -11,7 +11,8 @@ function [E] = euclidean(P)
 #save distance into Euclidic
   for i=1:n
     point = [P(i,1) P(i,2)];
-    distance =  sqrt(((R(1,1)-point(1,1))*(R(1,1)-point(1,1)))+ ((R(1,2)-P(1,2))*(R(1,2)-P(1,2))));
+
+    distance =  sqrt(((R(1,1)-point(1,1))*(R(1,1)-point(1,1)))+ ((R(1,2)-point(1,2))*(R(1,2)-point(1,2))));
     E(i,1) = distance;
   endfor
 endfunction
