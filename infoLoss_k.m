d05e356f94850edaf039016d17a2323e32ee56dd
Charlotte <burmeisc@tcd.ie>
2016-07-14 13:22:47 +0900

    Metrics added

---
+++ infoLoss_k.m
@@ -0,0 +1,9 @@
+function infoLoss_k(Origin,Mask,Index)
+
+Sum = 0;
+    rows = size(Origin,1);
+    for i=1:rows  
+      dist = sqrt((Origin(Index(i,1),1)-Mask(i,1))*(Origin(Index(i,1),1)-Mask(i,1)));
+      Sum= Sum+dist;
+    endfor
+endfunction
